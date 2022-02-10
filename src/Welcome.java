475299c642d70ce957a03cf478b9b241f681cd06
LachTomasz <lach.tomasz@gmail.com>
2022-02-10 21:48:27 +0100

    Ex3.1

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,2 +1,6 @@
 public class Welcome {
+    public static void main(String[] args) {
+        char[] Welcome = new char[] {'D','z','i','e','ń',' ','d','o','b','r','y'};
+        System.out.println(Welcome);
+    }
 }
